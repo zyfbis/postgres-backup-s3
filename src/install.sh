d928a12424331c111458aa93e86fe53a363f3ea7
zyfbis <zyfbis906@gmail.com>
2024-07-30 18:36:59 +0800

    add S3_VIRTUAL_HOSTING and TZ

--- src/install.sh
+++ src/install.sh
@@ -15,13 +15,13 @@ apk add aws-cli
 
 # install go-cron
 apk add curl
+apk add tzdata
 curl -L https://github.com/ivoronin/go-cron/releases/download/v0.0.5/go-cron_0.0.5_linux_${TARGETARCH}.tar.gz -O
 tar xvf go-cron_0.0.5_linux_${TARGETARCH}.tar.gz
 rm go-cron_0.0.5_linux_${TARGETARCH}.tar.gz
 mv go-cron /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
 apk del curl
-
 
 # cleanup
 rm -rf /var/cache/apk/*
--- src/run.sh
+++ src/run.sh
@@ -5,6 +5,9 @@ set -eu
 if [ "$S3_S3V4" = "yes" ]; then
   aws configure set default.s3.signature_version s3v4
 fi
+if [ "$S3_VIRTUAL_HOSTING" = "yes" ]; then
+  aws configure set default.s3.addressing_style virtual
+fi
 
 if [ -z "$SCHEDULE" ]; then
   sh backup.sh
